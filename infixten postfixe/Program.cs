1fed39538cea23b64f800eca89f22f73d4c8bd1c
Sencer Ozturk <muhammedsenceroztrk@gmail.com>
2025-02-06 03:03:15 +0300

    InfixToPostfix
    C# Code to Convert Infix to Postfix

---
+++ infixten postfixe/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infix_postfix_son_ders_kodları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Infix'ten Postfix'e çeviren kod
+            string postfix = "";
+            string infix = "a+b*c/d-e*f";
+            string op = "$+-*/()";
+            int[] oncelik = { 0, 2, 2, 3, 3, 1, 1 };
+
+            push("$");
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                int indis = op.IndexOf(infix[i]); //infix stringinin icinde op stringinin icinde olanlardan varsa onun ındeksını dondurur.
+
+                if (indis == -1)
+                {
+                    // Eğer karakter bir operatör değilse, doğrudan postfix'e ekle (yani harf gelmıs)
+                    postfix = postfix + infix[i];
+                    continue; //bir sonraki infix elemanına gec.
+                }
+
+                if (infix[i] == '(')
+                {
+                    push(infix[i].ToString()); //stacke push edıldı.
+                    continue;
+                }
+
+                if (infix[i] == ')')
+                {
+                    while (peek() != "(") //en sondakı mevcut  stacktekı ıfade acma parantezı olana dek.
+                    {
+                        postfix = postfix + pop(); //acma parantezı gelene kadar stackı bosalt.Pop cıkarılan degerı gerı dondurur.Parantez ıcını bosalttık.
+                    }
+                    pop(); // '(' karakterini yığıttan çıkar
+                    continue; //else yapısı kullanmak yerıne continue yapısı kullanılıyor.
+                }
+
+                // Operatörlerin önceliğine göre işlemler
+                while (oncelik[indis] <= oncelik[op.IndexOf(peek()[0])]) //eger gelen operand stackte bulunan operanddan kucuk ya da esıtse  [0] kısmını yazsaydık "" ıcınde bır strıng dondururdu ama bu op strıngının ıcınde yok yanı char aramalıyız o yuzden [0] koyduk.
+                {
+                    postfix = postfix + pop(); //pop yap kucuk ve esıt operandları
+                }
+
+                push(infix[i].ToString()); //Eger gelen eleman parantez olmadan ve mevcut stacktekı elemanlardan oncelıklı ıse dırekt ekle.
+            }
+
+            while (stackcount() > 1) //cevırme ıslemı bıttıgınde (for dongusu bıtınce) stacktekı butun elemanlar postfıxe eklenır
+            {
+                postfix = postfix + pop();
+            }
+
+            Console.WriteLine("Postfix: " + postfix);
+            Console.ReadKey();
+
+
+        }
+
+        // Stack fonksiyonları
+        static string[] stack = new string[100];
+        static int sp = -1;
+
+        static void push(string data)
+        {
+            sp++;
+            stack[sp] = data;
+        }
+
+        static string pop()
+        {
+            string data = stack[sp];
+            sp--;
+            return data;
+        }
+
+        static int stackcount()
+        {
+            return sp + 1;
+        }
+
+        static string peek() //stackte en son eklenen elemanı dondurur.
+        {
+            return stack[sp];
+        }
+
+
+
+
+
+  
+
+
+    }
+
+
+}
---
+++ infixten postfixe/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("infix postfix son ders kodları")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("infix postfix son ders kodları")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f01704c9-0f58-4b95-b351-584acb079e6f")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
